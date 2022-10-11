dd77d4bb2ed4c42b8e6b6b14f02c61aced99234d
August McAllister <august.chm@gmail.com>
2022-10-11 17:08:42 -0500

    Answered questions 5-10, began on 11

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -38,9 +38,9 @@ console.log('3 times 4 times 5 is:', multiplyThree(3,4,5));
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+    return false;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -52,15 +52,29 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  return array[array.length - 1];
 }
+
+let stuff = [ 'cat', 'rat', 'dog', 'fish'];
+let noStuff = [];
+
+console.log(getLast(stuff));
+console.log(getLast(noStuff));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+  for (item of array) {
+    if (item === value) {
+      return true;
+    }
+  }
+  return false;
 }
+
+console.log(find('dog', stuff));
+console.log(find('hamster', stuff));
 
 // ----------------------
 // Stretch Goals
@@ -68,24 +82,62 @@ function find( value, array ){
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === string[0]) {
+    return true;
+  }
+  return false;
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for (item of array) {
+    sum += item;
+  }
   return sum;
 }
+
+let numArray = [3, 4, 7, 23, 44]
+
+console.log(numArray);
+console.log('Sum of numArray:', sumAll(numArray));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+let posArray = [-3, -1, 0, 4, 8];
+
+function sumPositive(array) {
+  let sum = 0;
+  for (item of array) {
+    if (item > 0) {
+      sum += item;
+    }
+  }
+  return sum;
+}
+
+console.log(posArray);
+console.log('Sum of positive numbers in posArray:', sumPositive(posArray));
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+// let numString = 54321;
+
+// function digitize(n) {
+//   let array = [];
+//   for (i = 0; i < n.length; i++) {
+//     array.unshift(n[i]);
+//   }
+//   return array;
+// }
+
+// console.log(numString[2]); // expecting 3
+// console.log(digitize(54321)); // expecting [1, 2, 3, 4, 5]
