23b8d880f5c8b6be7b391a2e09222550028abe92
August McAllister <august.chm@gmail.com>
2022-10-11 11:58:33 -0500

    Answered questions 1-4

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,21 +15,23 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
+console.log(helloName('August'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers( firstNumber, secondNumber ) {
+  return firstNumber + secondNumber;
 }
+console.log('3 + 5 is:', addNumbers(3, 5));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( num1, num2, num3 ){
+  return num1 * num2 * num3;
 }
+console.log('3 times 4 times 5 is:', multiplyThree(3,4,5));
 
 
 // 5. Function that will return true if a number is positive, 
